c9c83ffdbdcc317fb3772a4557a32cef6cca5e28
Pires1945 <felipe.madeinbrazil@gmail.com>
2023-05-24 21:57:30 -0300

    Criada a tela de mensagens

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,6 @@
-import 'package:chat/core/services/auth/auth_mock_service.dart';
+import 'package:chat/components/messages.dart';
+import 'package:chat/components/new_messages.dart';
+import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -8,18 +10,48 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Chat'),
-          TextButton(
-              onPressed: () {
-                AuthMockService().logout();
+        appBar: AppBar(
+          title: Text('Chat'),
+          centerTitle: true,
+          actions: [
+            DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Container(
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Colors.black87,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text('Sair'),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
+                  AuthService().logout();
+                }
               },
-              child: Text('Logout'))
-        ],
-      ),
-    ));
+            ),
+          ],
+        ),
+        body: const SafeArea(
+            child: Column(
+          children: [
+            Expanded(child: Messages()),
+            NewMessage(),
+          ],
+        )));
   }
 }
